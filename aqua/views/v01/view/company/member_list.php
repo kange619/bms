7cda357e03c7cb61e411aa96b84f367ff776e0b8
kange619 <ksy619711@gmail.com>
2020-11-23 16:16:57 +0900

    11/23/20 kange 1. 페이지에 검색 옵션 추가    적용대상    - 가공원료 기준등록 (v_worked_materials_list)    - 원료기준등록 (v_raw_materials_list)    - 부자재기준등록 (v_materials_list)    - 생산제품등록 (v_production_goods_list)    - 생산담당자관리 (v_production_member_list)    - HACCP담당자관리 (v_member_list)

--- aqua/views/v01/view/company/member_list.php
+++ aqua/views/v01/view/company/member_list.php
@@ -23,53 +23,49 @@
                                 <input type="hidden" name="sch_order_field" id="sch_order_field" value="<?=$sch_order_field?>">
                                 <input type="hidden" name="sch_order_status" id="sch_order_status" value="<?=$sch_order_status?>">
 
-
+                                <!-- 11/23/20 kange Add 검색필터 추가 -->
                                 <div class="col-sm-4">
                                     <div class="form-group">
                                         <label class="col-sm-3 control-label">가입일</label>
-                                        <div class="col-sm-9">
-                                            <div class="input-daterange input-group" id="date-range">
-                                                <input type="text" class="form-control datepicker" readonly="readonly" name="sch_s_date" value="<?=$sch_s_date;?>">
-                                                <span class="input-group-addon bg-primary b-0 text-white">~</span>
-                                                <input type="text" class="form-control datepicker" readonly="readonly" name="sch_e_date" value="<?=$sch_e_date;?>">
-                                            </div>
+
+                                        <div class="input-daterange input-group" id="date-range">
+                                            <input type="text" class="form-control datepicker" readonly="readonly" name="sch_s_date" value="<?=$sch_s_date;?>">
+                                            <span class="input-group-addon bg-primary b-0 text-white">~</span>
+                                            <input type="text" class="form-control datepicker" readonly="readonly" name="sch_e_date" value="<?=$sch_e_date;?>">
+                                        </div>
+
+                                    </div>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label class="col-sm-2 control-label">상태</label>
+                                        <div class="col-sm-2">
+
+                                            <select class="form-control" name="sch_use_flag" style="width:200px" >
+                                                <option value="" <?=($sch_use_flag == '' ? 'selected="selected"' : '' )?> >전체</option>
+                                                <option value="Y" <?=($sch_use_flag == 'Y' ? 'selected="selected"' : '' )?> >사용</option>
+                                                <option value="N" <?=($sch_use_flag == 'N' ? 'selected="selected"' : '' )?> >미사용</option>
+                                            </select>
+
+                                        </div>
+                                        <div class="col-sm-7">
+                                            <input type="text" class="form-control" name="sch_keyword" value="<?=$sch_keyword;?>" placeholder="담당자명, 휴대폰번호호">
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="col-sm-4">
-                                    <div class="form-group">
-                                        <label class="col-sm-3 control-label">상태</label>
-                                        <div class="col-sm-9">
-                                            <div class="input-daterange input-group" id="date-range">
-                                                <select class="form-control" name="sch_use_flag" style="width:200px" >                                                
-                                                <option value="" <?=($sch_use_flag == '' ? 'selected="selected"' : '' )?> >전체</option>
-                                                <option value="Y" <?=($sch_use_flag == 'Y' ? 'selected="selected"' : '' )?> >사용</option>
-                                                <option value="N" <?=($sch_use_flag == 'N' ? 'selected="selected"' : '' )?> >미사용</option>
-                                                
-                                            </select>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                
-                                <div class="col-sm-6">
-                                    <div class="form-group">
-                                        <label class="col-sm-2 control-label">검색</label>
-                                        <div class="col-sm-9">
-                                            <input type="text" class="form-control" name="sch_keyword" value="<?=$sch_keyword;?>" placeholder="담당자명, 휴대폰번호">
-                                        </div>
-                                    </div>
-                                </div>
                                 <div class="col-md-12">
                                     <div class="form-group">
                                         <div class="col-sm-offset-5 col-sm-7 m-t-15">
                                             <button type="button" class="btn btn-primary waves-effect waves-light" onclick="location.href='./<?=$page_name?>_list?top_code=<?=$top_code?>&left_code=<?=$left_code?>'">기본설정</button>
-                                            <!-- <button type="reset" class="btn btn-primary waves-effect waves-light">기본설정</button> -->
-                                            <button type="submit" class="btn btn-inverse waves-effect m-l-5" onclick="location.href='./<?=$page_name?>_list?top_code=<?=$top_code?>&left_code=<?=$left_code?>&sch_keyword=<?$sch_keyword?>'">검색</button>
+                                            <button type="submit" class="btn btn-inverse waves-effect m-l-5">검색</button>
                                         </div>
                                     </div>
                                 </div>
+                                <!-- 11/23/20 kange Add 검색필터 추가 -->
+
+
                             </form>
                         </div>
                     </div>
